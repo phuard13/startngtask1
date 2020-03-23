af3b6d2921dcc53468df6fb6e66a47f0a8a33508
Fouhad <51534299+phuard13@users.noreply.github.com>
2020-03-23 17:28:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Fouhad Olalekan";
+const courses = ["frontend", "Backend", "HTML", "CSS", "Javascript", "Nordjs"];
+
+console.log(name);
+console.log(courses);
+
+for (let i = 1; i <= 200; i++) {
+  if (i % 2 === 0) console.log(i);
+}
